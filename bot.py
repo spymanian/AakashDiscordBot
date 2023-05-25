0ea6621fead78e36c85c1d9d85878ce824bcfb98
spymanian <90417628+spymanian@users.noreply.github.com>
2023-05-25 19:33:13 -0400

    DiscordBot

---
+++ bot.py
@@ -0,0 +1,42 @@
+import discord
+import responses
+# from keep_up import keep_awake
+
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = 'MTExMTMxODY5ODc5MjM0MTYxNA.GZDODY.X9O1KRIfCEp8CR4n9QIntPBpcDtc0LtozE1NoI'
+    intents = discord.Intents.all()
+    intents.message_content = True
+    intents.members = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}'({channel})")
+        if len(user_message) > 0:
+            if user_message[0] == '?':
+                user_message = user_message[1:]
+                await send_message(message, user_message, is_private=True)
+            else:
+                await send_message(message, user_message, is_private=False)
+
+    # keep_awake()
+    client.run(TOKEN)
---
+++ keep_up.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "Hello. I am awake!"
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+# function to keep the bot awake
+def keep_awake():
+    t = Thread(target=run)
+    t.start()
